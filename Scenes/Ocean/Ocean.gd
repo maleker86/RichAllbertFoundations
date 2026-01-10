8ee60a45d5c48f9fe7b2d9c843562b21a91502f2
maleker86 <109919530+maleker86@users.noreply.github.com>
2026-01-10 15:01:12 -0800

    complete Look At section

--- Scenes/Ocean/Ocean.gd
+++ Scenes/Ocean/Ocean.gd
@@ -1,6 +1,13 @@
-extends Node
+extends Node2D
 
 @onready var plane: Sprite2D = $Plane
+@onready var helicopter: Sprite2D = $Helicopter
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("SetTarget"):
+		plane.look_at(get_global_mouse_position())
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +16,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.position.x -= 50.0 * delta
+	#plane.global_position.y += 50.0 * delta
+	helicopter.global_position.x += 50.0 * delta;
+	
