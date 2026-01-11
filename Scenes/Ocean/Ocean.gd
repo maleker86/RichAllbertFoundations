3b3ad65ed9c8e2bdefe4f7049ccea984d7f0f52b
maleker86 <109919530+maleker86@users.noreply.github.com>
2026-01-10 20:34:48 -0800

    allow plane to rotate/progress with UI_R, L, U

--- Scenes/Ocean/Ocean.gd
+++ Scenes/Ocean/Ocean.gd
@@ -1,19 +1,17 @@
 extends Node2D
 
 @onready var plane: Sprite2D = $Plane
-@onready var helicopter: Sprite2D = $Helicopter
+#@onready var helicopter: Sprite2D = $Helicopter
 
-var speed: float = 50.0
+var speed: float = 100.0
+const ROT_SPEED: float = PI / 2;
 
+#var _target: Vector2 = Vector2.ZERO
 
-var _target: Vector2 = Vector2.ZERO
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	_target = get_global_mouse_position()
-	if event.is_action_pressed("SetTarget"):
-		plane.look_at(_target)
-
+#func _unhandled_input(event: InputEvent) -> void:
+	#_target = get_global_mouse_position()
+	#if event.is_action_pressed("SetTarget"):
+		#plane.look_at(_target)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,5 +24,11 @@ func _process(delta: float) -> void:
 	#helicopter.global_position.x += 50.0 * delta;
 	#plane.position = plane.position.move_toward(_target, 50.0 * delta)
 	#plane.position += plane.transform.x.normalized() * speed * delta
-	plane.move_local_x(speed * delta)
-	pass
+	if Input.is_action_pressed("ui_up"):
+		plane.move_local_x(speed * delta)
+		
+	if Input.is_action_pressed("ui_right"):
+		plane.rotate(ROT_SPEED * delta)
+		
+	if Input.is_action_pressed("ui_left"):
+		plane.rotate(-ROT_SPEED * delta)
